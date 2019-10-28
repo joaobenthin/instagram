0cd180f4ef074ab9f8281a6a342372251dad53f1
joaobenthin <joaopaulobenthin@hotmail.com>
2019-10-28 15:48:16 -0300

    lazy loading

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, FlatList } from 'react-native';
 
 import LazyImage from '~/components/LazyImage';
@@ -11,6 +11,7 @@ export default function Feed() {
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [viewable, setViewable] = useState([]);
 
   async function loadPage(pageNumber = page, shouldRefresh = false) {
     if (total && pageNumber > total) return;
@@ -43,6 +44,10 @@ export default function Feed() {
     setRefreshing(false);
   }
 
+  const handleViewableChanged = useCallback(({ changed }) => {
+    setViewable(changed.map(({ item }) => item.id));
+  }, []);
+
   return (
     <View>
       <FlatList
@@ -52,6 +57,8 @@ export default function Feed() {
         onEndReachedThreshold={0.1}
         onRefresh={refreshList}
         refreshing={refreshing}
+        onViewableItemsChanged={handleViewableChanged}
+        viewabilityConfig={{ viewAreaCoveragePercentThreshold: 10 }}
         ListFooterComponent={loading && <Loading />}
         renderItem={({ item }) => (
           <Post>
@@ -61,6 +68,7 @@ export default function Feed() {
             </Header>
 
             <LazyImage
+              shouldLoad={viewable.includes(item.id)}
               aspectRatio={item.aspectRatio}
               smallSource={{ uri: item.small }}
               source={{ uri: item.image }}
